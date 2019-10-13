0e4c667d2646353edc9df3c088aa8dba5855f46d
Mauricio Duarte <prussdev@gmail.com>
2019-10-13 16:42:31 -0300

    Projeto criado

---
+++ src/main/java/com/mauriciopd/carstore/ProjectCarStoreApplication.java
@@ -0,0 +1,13 @@
+package com.mauriciopd.carstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectCarStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectCarStoreApplication.class, args);
+	}
+
+}
