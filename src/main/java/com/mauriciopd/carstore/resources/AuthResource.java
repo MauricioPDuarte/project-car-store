a2e5a7f90b76844f88754f75b386c310bb10674c
Mauricio Duarte <prussdev@gmail.com>
2019-10-25 17:16:46 -0300

    Refresh token

---
+++ src/main/java/com/mauriciopd/carstore/resources/AuthResource.java
@@ -0,0 +1,29 @@
+package com.mauriciopd.carstore.resources;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.mauriciopd.carstore.security.JWTUtil;
+import com.mauriciopd.carstore.security.UserSS;
+import com.mauriciopd.carstore.services.UserService;
+
+@RestController
+@RequestMapping(value = "/auth")
+public class AuthResource {
+
+	@Autowired
+	private JWTUtil jwtUtil;
+
+	@PostMapping("/refresh_token")
+	public ResponseEntity<Void> refreshToken(HttpServletResponse response) {
+		UserSS user = UserService.authenticated();
+		String token = jwtUtil.generateToken(user.getUsername());
+		response.addHeader("Authorization", "Bearer " + token);
+		return ResponseEntity.noContent().build();
+	}
+}
